3bc16356dd38852688a98cd6192e48c7b8ef71ce
Bryton Lacquement <contact@bminusl.xyz>
2020-09-26 19:09:44 +0200

    sheet: show the total amount per category, with currency
    Also some other design changes

--- sheets/templatetags/sheet_extras.py
+++ sheets/templatetags/sheet_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
@@ -6,3 +7,14 @@ register = template.Library()
 @register.filter
 def order_queryset_by(queryset, order):
     return queryset.order_by(order)
+
+
+@register.filter
+def attrsum(container, attr_name):
+    return sum(getattr(e, attr_name) for e in container)
+
+
+@register.filter
+@stringfilter
+def currency(string):
+    return f"{string.replace('.', ',')}€"  # TEMP:
