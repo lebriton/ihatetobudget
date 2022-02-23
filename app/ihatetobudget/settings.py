2cde5cd603e6ca7011a3370ce66d849dda3de128
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2022-02-23 12:38:53 +0100

    move database dir to allow persistence

--- app/ihatetobudget/settings.py
+++ app/ihatetobudget/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = "ihatetobudget.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "data" / "db.sqlite3",
     }
 }
 
