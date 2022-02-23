43813099efc15a4380ff788345c34b8c1bca8c74
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2022-02-23 12:20:31 +0100

    add 'asgimiddlewarestaticfile' package to remove need for Caddy

--- app/ihatetobudget/asgi.py
+++ app/ihatetobudget/asgi.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+from django.conf import settings
 from django.core.asgi import get_asgi_application
+from asgi_middleware_static_file import ASGIMiddlewareStaticFile
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ihatetobudget.settings")
 
 application = get_asgi_application()
+application = ASGIMiddlewareStaticFile(
+    application,
+    static_url=settings.STATIC_URL,
+    static_root_paths=[settings.STATIC_ROOT],
+)
